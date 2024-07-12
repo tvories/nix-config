aee425835248a0acf3e50ebcb89b8c4018b60016
taylor <taylor@tmtech.me>
2024-07-11 22:17:13 -0600

    feat: add gcloud

--- homes/_modules/shell/gcloud/default.nix
+++ homes/_modules/shell/gcloud/default.nix
@@ -1,14 +1,23 @@
 {
   pkgs,
+  config,
+  lib,
   ...
 }:
 let
+  cfg = config.modules.shell.gcloud;
   gdk = pkgs.google-cloud-sdk.withExtraComponents( with pkgs.google-cloud-sdk.components; [
     gke-gcloud-auth-plugin
   ]);
-in
-{
-  packages = [
-    gdk
+in {
+  options.modules.shell.gcloud = {
+    enable = lib.mkEnableOption "gcloud";
+  };
+  config = lib.mkMerge [
+    (lib.mkIf cfg.enable {
+      home.packages = [
+        gdk
+      ];
+    })
   ];
 }

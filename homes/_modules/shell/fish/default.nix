407f8c8e48a21c64760a681d65a21d29708c665e
Taylor Vories <taylor@tmtech.me>
2024-12-18 15:47:14 -0700

    chore: small updates
    Signed-off-by: Taylor Vories <taylor@tmtech.me>

--- homes/_modules/shell/fish/default.nix
+++ homes/_modules/shell/fish/default.nix
@@ -8,10 +8,10 @@ let
   inherit (pkgs.stdenv.hostPlatform) isDarwin;
   inherit (config.home) username homeDirectory;
   cfg = config.modules.shell.fish;
-  hasPackage = pname:
-    lib.any (p: p ? pname && p.pname == pname) config.home.packages;
+  hasPackage = pname: lib.any (p: p ? pname && p.pname == pname) config.home.packages;
   hasAnyNixShell = hasPackage "any-nix-shell";
-in {
+in
+{
   options.modules.shell.fish = {
     enable = lib.mkEnableOption "fish";
   };
@@ -23,43 +23,52 @@ in {
         catppuccin.enable = true;
 
         plugins = [
-          { name = "done"; inherit (pkgs.fishPlugins.done) src; }
-          { name = "puffer"; inherit (pkgs.fishPlugins.puffer) src; }
+          {
+            name = "done";
+            inherit (pkgs.fishPlugins.done) src;
+          }
+          {
+            name = "puffer";
+            inherit (pkgs.fishPlugins.puffer) src;
+          }
         ];
 
-        interactiveShellInit = ''
-          function remove_path
-            if set -l index (contains -i $argv[1] $PATH)
-              set --erase --universal fish_user_paths[$index]
-            end
-          end
-
-          function update_path
-            if test -d $argv[1]
-              fish_add_path -m $argv[1]
-            else
-              remove_path $argv[1]
-            end
-          end
-
-          # Paths are in reverse priority order
-          update_path /opt/homebrew/bin
-          update_path ${homeDirectory}/.krew/bin
-          update_path /nix/var/nix/profiles/default/bin
-          update_path /run/current-system/sw/bin
-          update_path /etc/profiles/per-user/${username}/bin
-          update_path /run/wrappers/bin
-          update_path ${homeDirectory}/.nix-profile/bin
-          update_path ${homeDirectory}/go/bin
-          update_path ${homeDirectory}/.cargo/bin
-          update_path ${homeDirectory}/.local/bin
-        '' + (
-          if hasAnyNixShell
-          then ''
-            any-nix-shell fish --info-right | source
+        interactiveShellInit =
           ''
-          else ""
-        );
+            function remove_path
+              if set -l index (contains -i $argv[1] $PATH)
+                set --erase --universal fish_user_paths[$index]
+              end
+            end
+
+            function update_path
+              if test -d $argv[1]
+                fish_add_path -m $argv[1]
+              else
+                remove_path $argv[1]
+              end
+            end
+
+            # Paths are in reverse priority order
+            update_path /opt/homebrew/bin
+            update_path ${homeDirectory}/.krew/bin
+            update_path /nix/var/nix/profiles/default/bin
+            update_path /run/current-system/sw/bin
+            update_path /etc/profiles/per-user/${username}/bin
+            update_path /run/wrappers/bin
+            # update_path ${homeDirectory}/.nix-profile/bin
+            update_path ${homeDirectory}/go/bin
+            update_path ${homeDirectory}/.cargo/bin
+            update_path ${homeDirectory}/.local/bin
+          ''
+          + (
+            if hasAnyNixShell then
+              ''
+                any-nix-shell fish --info-right | source
+              ''
+            else
+              ""
+          );
       };
 
       home.sessionVariables.fish_greeting = "";

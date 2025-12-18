e8070f6fb78b1728ad17c3cefaff57129a8c0a2d
Taylor Vories <taylor@tmtech.me>
2025-12-17 23:51:46 -0700

    fix: zerobyte
    Signed-off-by: Taylor Vories <taylor@tmtech.me>

--- hosts/nas3/zerobyte.nix
+++ hosts/nas3/zerobyte.nix
@@ -13,9 +13,9 @@
         image = "ghcr.io/nicotsx/zerobyte:v0.19";
         ports = [ "4096:4096" ];
         autoStart = "true";
-        environment = [
-          "TZ=America/Denver"
-        ];
+        environment = {
+          TZ = "America/Denver";
+        };
         volumes = [
           "/etc/localtime:/etc/localtime:ro"
           "/var/lib/zerobyte:/var/lib/zerobyte"

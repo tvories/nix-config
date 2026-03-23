8c0c7988031e0c847c66a6d3b519a6ee71d08a0a
tvories <taylor@tmtech.me>
2026-03-23 12:26:35 -0600

    feat: update flake
    Signed-off-by: tvories <taylor@tmtech.me>

--- flake.nix
+++ flake.nix
@@ -1,6 +1,23 @@
 {
   description = "taylor Nix Flake";
 
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+      "https://cache.garnix.io"
+      "https://numtide.cachix.org"
+      "https://bjw-s.cachix.org"
+      "https://tvories.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+      "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+      "bjw-s.cachix.org-1:dWyzjMYlnKeq01PplRWadakXfZQBoxJN7zGO6/HwsPs="
+      "tvories.cachix.org-1:Ql12AwHIbutZhu6fs3Ld29JfKWBZtZvu5araZnwcT+E="
+    ];
+  };
+
   inputs = {
     # Nixpkgs and unstable
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";

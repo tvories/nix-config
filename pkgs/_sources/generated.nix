552e87c7bdacf39ce6752233ddcdf5cb568299e2
tvories <13934524+tvories@users.noreply.github.com>
2026-09-13 03:40:27 +0000

    chore(deps): Update nvfetcher sources

--- pkgs/_sources/generated.nix
+++ pkgs/_sources/generated.nix
@@ -19,13 +19,13 @@
   };
   usage-cli = {
     pname = "usage-cli";
-    version = "v6.7.1";
+    version = "v6.9.0";
     src = fetchFromGitHub {
       owner = "jdx";
       repo = "usage";
-      rev = "v6.7.1";
+      rev = "v6.9.0";
       fetchSubmodules = false;
-      sha256 = "sha256-dnMDPREzXm6Tp5hbk100iCGJDlDqu1S36Dn7fH8eRuY=";
+      sha256 = "sha256-LMgvgmh32CkbKzC6h/F6oYdI5iK2gdaCuoY2dVJrQp0=";
     };
   };
 }
